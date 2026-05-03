852950212441e8590f06733948a022d703ad2146
Gleb Kiva <feironox5@gmail.com>
2026-05-03 11:31:30 +0300

    ui: list center content

--- projects/webapp/src/shared/ui/bits/List.vue
+++ projects/webapp/src/shared/ui/bits/List.vue
@@ -6,6 +6,10 @@
     <slot name="start" />
     <slot />
     <span class="spacer" />
+    <span class="center">
+      <slot name="center" />
+    </span>
+    <span class="spacer" />
     <slot name="end" />
   </div>
 </template>
@@ -19,5 +23,22 @@ defineProps(listProps);
 <style scoped>
 .list {
   gap: var(--s-spacing);
+
+  & .center {
+    display: contents;
+  }
+
+  &.row {
+    position: relative;
+
+    & .center {
+      display: inline-flex;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      align-self: center;
+    }
+  }
 }
 </style>
