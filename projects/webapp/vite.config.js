74d9deb0df36751bbd8f903cf568b6f583a803de
Gleb Kiva <feironox5@gmail.com>
2026-05-17 14:55:27 +0300

    ci: format vite.config.js

--- projects/webapp/vite.config.js
+++ projects/webapp/vite.config.js
@@ -7,14 +7,56 @@ export default defineConfig({
   plugins: [vue(), themePlugin()],
   resolve: {
     alias: [
-      { find: '@/app', replacement: path.resolve(__dirname, './src/app') },
-      { find: '@/ui', replacement: path.resolve(__dirname, './src/app/ui') },
-      { find: '@/types', replacement: path.resolve(__dirname, './src/app/types') },
-      { find: '@', replacement: path.resolve(__dirname, './src') },
-      { find: '@shared', replacement: path.resolve(__dirname, './src/shared') },
-      { find: '@bits', replacement: path.resolve(__dirname, './src/shared/ui/bits') },
-      { find: '@components', replacement: path.resolve(__dirname, './src/shared/ui/components') },
-      { find: '@netop/types', replacement: path.resolve(__dirname, '../types/src/index.ts') },
+      {
+        find: '@/app',
+        replacement: path.resolve(__dirname, './src/app'),
+      },
+      {
+        find: '@/ui',
+        replacement: path.resolve(
+          __dirname,
+          './src/app/ui',
+        ),
+      },
+      {
+        find: '@/types',
+        replacement: path.resolve(
+          __dirname,
+          './src/app/types',
+        ),
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src'),
+      },
+      {
+        find: '@shared',
+        replacement: path.resolve(
+          __dirname,
+          './src/shared',
+        ),
+      },
+      {
+        find: '@bits',
+        replacement: path.resolve(
+          __dirname,
+          './src/shared/ui/bits',
+        ),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(
+          __dirname,
+          './src/shared/ui/components',
+        ),
+      },
+      {
+        find: '@netop/types',
+        replacement: path.resolve(
+          __dirname,
+          '../types/src/index.ts',
+        ),
+      },
     ],
     tsConfigPaths: true,
   },
